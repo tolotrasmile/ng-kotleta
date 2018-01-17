eef39f7d011a8bc40ad23daa62ef728ef3238c51
Lovasoa Arnaud <lovasoa.arnaud@gmail.com>
2018-01-17 10:38:27 +0300

    Add Components : posts-list and users-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component'
 import { UserComponent } from './components/user/user.component'
-import { UserService } from './services/user.service';
-import { PostComponent } from './components/post/post.component'
+import { UserService } from './services/user.service'
+import { PostComponent } from './components/post/post.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
+import { PostsListComponent } from './components/posts-list/posts-list.component'
 
 @NgModule({
-  declarations: [AppComponent, UserComponent, PostComponent],
+  declarations: [AppComponent, UserComponent, PostComponent, UsersListComponent, PostsListComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [UserService],
   bootstrap: [AppComponent]
